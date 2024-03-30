d14840bbf69bc223c6a12ca41ffd7b2a3d63d34c
martin <mwirigi.8kabush@gmail.com>
2024-03-29 23:21:21 -0700

    all_excutables

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_str = ""
-    for i in str:
-        if i >= 'a' and i <= 'z':
-            new_str = new_str + chr((ord(i) - 32))
-        else:
-            new_str = new_str + i
-            print("{}".format(new_str))
+    for letter in str:
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end="")
+    print()
