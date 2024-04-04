2e9944626cd27a56e9895ce34a3d781c44464635
martin <mwirigi.8kabush@gmail.com>
2024-04-03 21:48:35 -0700

    sorted linked list

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct listint_s - singly linked list
@@ -13,11 +13,13 @@ typedef struct listint_s
 {
     int n;
     struct listint_s *next;
-} listint_t;
+} listint_t
+
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-listint_t *insert_node(listint_t **head, int number);
 void free_listint(listint_t *head);
 
-#endif /* _LISTS_H_ */
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
