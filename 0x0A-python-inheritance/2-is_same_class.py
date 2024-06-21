cf31c85b9995f5d1b9d1d57c37182aa2dfe61b68
martin <mwirigi.8kabush@gmail.com>
2024-06-20 19:48:46 -0700

    Integer validator

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""a function that returns True if the object is exactly an
-   instance of the specified class ; otherwise False."""
+"""
+This module contains the function is_same_class
+"""
 
 
 def is_same_class(obj, a_class):
-    """returns True if the object is exactly an instance of
-       the specified class ; otherwise False"""
+    """return true if obj is the exact class a_class, otherwise false"""
     return (type(obj) == a_class)
