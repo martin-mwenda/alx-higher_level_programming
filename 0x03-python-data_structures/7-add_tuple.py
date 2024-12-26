a0756c0477136b5746fb13d5fb7c7034d287265d
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-12-25 20:25:10 -0800

    changes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,11 +12,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple: A tuple containing the sum of corresponding elements from
                tuple_a and tuple_b.
     """
-    # Ensure both tuples are at least 2 elements long by padding with (0, 0)
-    tuple_1 = tuple_a + (0, 0)
-    tuple_2 = tuple_b + (0, 0)
-
-    # Add corresponding elements
-    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
-
-    return new_tuple
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
