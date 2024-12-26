d775af50904483e38a185f97618c0a7de71bef3d
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-12-25 20:17:48 -0800

    . Find the max

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -9,5 +9,10 @@ def divisible_by_2(my_list=[]):
         list: A list of booleans, where True indicates that the integer at
               that position is divisible by 2, and False otherwise.
     """
-    new_list = [not i % 2 for i in my_list]
-    return (new_list)
+     new = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new.append(True)
+        else:
+            new.append(False)
+    return (new)
