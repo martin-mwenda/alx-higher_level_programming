376877412dfe8b05ef04fdebc527cdf900fbf3a6
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-12-25 20:21:59 -0800

    changes

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -9,7 +9,7 @@ def divisible_by_2(my_list=[]):
         list: A list of booleans, where True indicates that the integer at
               that position is divisible by 2, and False otherwise.
     """
-     new = []
+    new = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             new.append(True)
